9544df29f05ebbf54604f67561483fd4e7bbd157
aliandanas28-sys <aliandanas28@gmail.com>
2026-03-27 00:18:30 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/', createProxyMiddleware({
+    target: 'https://generativelanguage.googleapis.com',
+    changeOrigin: true,
+    onProxyRes: function (proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+    }
+}));
+
+module.exports = app;
