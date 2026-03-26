154cbe738e458c8e53fdeb29bc0fb45669f2ed0e
aliandanas28-sys <aliandanas28@gmail.com>
2026-03-27 00:56:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (req, res) => {
-  // هذا التعديل سيجعل السيرفر يوجه الطلب فوراً لـ Google Gemini
   const proxy = createProxyMiddleware({
     target: 'https://generativelanguage.googleapis.com',
     changeOrigin: true,
-    pathRewrite: (path) => path, // سيمرر المسار كما هو
+    pathRewrite: (path) => path,
   });
 
   return proxy(req, res);
